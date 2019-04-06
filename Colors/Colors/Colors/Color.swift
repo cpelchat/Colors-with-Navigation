8919e38805412a9be6e45f1030816dc4c90bce27
Cassidy Pelchat <42526462+cpelchat@users.noreply.github.com>
2019-04-05 23:43:51 -0500

    Add files via upload

---
+++ Colors/Colors/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Cassidy Pelchat on 4/5/19.
+//  Copyright © 2019 Cassidy Pelchat. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name : String
+    let uiColor: UIColor
+}
